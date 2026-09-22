b38d48fe9cf8193ab11f6627cb7129ac731a01d0
karina k machado <karina_k_machado@estudante.sesisenai.org.br>
2026-09-22 20:28:37 -0300

    adição da atividade 01

--- atividade01.js
+++ atividade01.js
@@ -2,9 +2,7 @@ const prompt = require('prompt-sync')();
 
 let Itens = [];
 
-// ===========================
 // ETAPA 2 - Regra de negócio
-// ===========================
 
 function RegraNegocio(preco) {
     if (preco < 100) {
@@ -16,50 +14,44 @@ function RegraNegocio(preco) {
     }
 }
 
-// ===========================
 // ETAPA 3 - Cadastro
-// ===========================
 
 for (let i = 0; i < 5; i++) {
     let nomeItem = "";
 
-// Validação de dados
+    // Validação de dados
 
     while (nomeItem.trim() === "") {
-        nomeItem = prompt(`Insira o nome do ${i+1}º item: `);
+        nomeItem = prompt(`Insira o nome do ${i + 1}º item: `);
     }
 
-let precoItem = NaN;
+    let precoItem = NaN;
 
     while (isNaN(precoItem) || precoItem <= 0) {
-        precoItem = parseFloat(prompt(`Insira o preço do ${i+1}º item: `));
+        precoItem = parseFloat(prompt(`Insira o preço do ${i + 1}º item: `));
     }
 
-let raridadeItem = RegraNegocio(precoItem);
+    let raridadeItem = RegraNegocio(precoItem);
 
-// ===========================
-// ETAPA 1 - Ficha do item
-// ===========================
+    // ETAPA 1 - Ficha do item
 
-// Criação do item
+    // Criação do item
 
     let Item = {
         Nome: nomeItem,
         Preco: precoItem,
         Raridade: raridadeItem,
         Estoque: 12,
-        Promocao: ((i+1) % 2 === 0),
+        Promocao: ((i + 1) % 2 === 0),
         Destaque: (precoItem > 500)
     };
 
-// Guardar na array 
+    // Guardar na array 
 
     Itens.push(Item);
 }
 
-// ===========================
 // ETAPA 4 - Estoque (while)
-// ===========================
 
 console.table(Itens);
 
@@ -81,9 +73,7 @@ while (itemSelecionado.Estoque > 0) {
     console.log(`Vendeu o item ${itemSelecionado.Nome}, quantidade restante: ${itemSelecionado.Estoque}`);
 }
 
-// ===========================
 // ETAPA 5 - Tabela com os itens
-// ===========================
 
 // Chamado novamente para mostrar que o item vendido zerou
 
